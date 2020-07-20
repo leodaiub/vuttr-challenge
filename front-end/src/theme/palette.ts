320eb4c9c0bef8ff69ca7486858a3a8ae88acfab
Leonardo <leodaiub@gmail.com>
2020-07-20 19:07:15 -0300

    feat: :sparkles: light/dark theme

---
+++ front-end/src/theme/palette.ts
@@ -0,0 +1,42 @@
+import { colors } from '@material-ui/core';
+
+export default {
+  primary: {
+    dark: colors.grey[200],
+    main: '#88EDC4',
+    light: colors.grey[200],
+  },
+  secondary: {
+    dark: colors.grey[200],
+    main: '#365DF0',
+    light: colors.blue['A400'],
+  },
+  success: {
+    dark: '#FCAEAC',
+    main: '#FCAEAC',
+    light: '#88EDC4',
+  },
+  info: {
+    dark: '#88EDC4',
+    main: '#365DF0',
+    light: '#365DF0',
+  },
+  warning: {
+    dark: '#D89C3A',
+    main: '#FFBB43',
+    light: '#FFBB43',
+  },
+  error: {
+    dark: '#CC4C4C',
+    main: '#F95E5A',
+    light: '#FCAEAC',
+  },
+  text: {
+    // primary: colors.grey[200],
+    // secondary: colors.grey[200],
+    link: colors.green[600],
+  },
+
+  icon: '#170C3A',
+  divider: colors.grey[200],
+};
